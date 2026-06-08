d50c5b5a86a0f8b6a4202c030ca65de357274d47
Arbon Kastrati <arbon_kastrati@outlook.com>
2026-06-08 16:37:16 +0200

    add Modal.js component through which we handle the opening and closing of the modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,29 @@
+class Modal {
+    constructor() {
+        this.modal = document.getElementById('modal');
+        this.modalBtn = document.getElementById('modal-btn');
+        this.attachListeners();
+    }
+
+    attachListeners() {
+        this.modalBtn.addEventListener('click', this.show.bind(this));
+        document.addEventListener('click', this.outsideClick.bind(this));
+        document.addEventListener('closemodal', this.close.bind(this));
+    }
+
+    show() {
+        this.modal.style.display = 'block';
+    }
+
+    close() {
+        this.modal.style.display = 'none';
+    }
+
+    outsideClick(e) {
+        if (e.target === this.modal) {
+            this.close();
+        }
+    }
+}
+
+export default Modal;
