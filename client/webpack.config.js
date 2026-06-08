8f20a1fb9b52ce0dde910bd5a4a0f1f6ad5e4ad2
Arbon Kastrati <arbon_kastrati@outlook.com>
2026-06-08 15:49:06 +0200

    Initial stage of the client-side part of the app

---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+console.log(__dirname);
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, '../public'),
+        },
+        port: 9000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+};
